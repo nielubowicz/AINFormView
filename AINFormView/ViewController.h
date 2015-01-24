84667c8ac53aa6f631fb04a9084cd052af351f56
nielubowicz <nielubowicz@gmail.com>
2015-01-23 21:46:09 -0500

    Initial Commit

---
+++ AINFormView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AINFormView
+//
+//  Created by chris nielubowicz on 1/23/15.
+//  Copyright (c) 2015 Assorted Intelligence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AINFormView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AINFormView
+//
+//  Created by chris nielubowicz on 1/23/15.
+//  Copyright (c) 2015 Assorted Intelligence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
